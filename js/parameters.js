aad705d6c1d40c4fb7b2e137b29ec87c4b1d9da8
Alberto <alberto.alarcon@tech-impulse.com>
2014-05-21 15:31:51 +0200

    no message

--- js/parameters.js
+++ js/parameters.js
@@ -5,7 +5,7 @@ var token = "";
 var versionApp = "1.1.0"; //  16/05/2014
 var tiempoRecargaBD = 360; // Horas en minutos para recargar la base de datos
 var transactionId = "";
-var tiempoSincronismo= 30000;
+var tiempoSincronismo= 400000;
 
 var TIPO_TEMPORAL_ORDER=0;
 var TIPO_TEMPORAL_TEMPLATE=1;
